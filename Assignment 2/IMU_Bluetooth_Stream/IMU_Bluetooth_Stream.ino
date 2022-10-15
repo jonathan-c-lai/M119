c4c4e7790d8afe6b5fe029482828d28f9292d7b0
Jonathan Lai <78984450+jonathan-c-lai@users.noreply.github.com>
2022-10-15 16:35:09 -0700

    Completed Req 1, but working on Req 2
    For Req 2,
    need to figure out how to send decimals to phone and also how to write multiple values at the same time (or if this is necessary)

---
+++ Assignment 2/IMU_Bluetooth_Stream/IMU_Bluetooth_Stream.ino
@@ -0,0 +1,141 @@
+#include <ArduinoBLE.h>
+#include <Arduino_LSM6DS3.h>
+
+BLEService ledService("180A"); // BLE LED Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("2A57", BLERead | BLEWrite);
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // set built in LED pin to output mode
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // initialize IMU
+  if (!IMU.begin()) {
+    Serial.println("IMU initialization failed");
+
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in g's");
+  Serial.println("X\tY\tZ");
+
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Gyroscope in degrees/second");
+  Serial.println("X\tY\tZ");
+
+  // initialize bluetooth
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy failed!");
+
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("Nano 33 IoT");
+  BLE.setAdvertisedService(ledService);
+
+  // add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+
+  // add service
+  BLE.addService(ledService);
+
+  // set the initial value for the characteristic:
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE LED Peripheral");
+}
+
+void loop() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+      // setup IMU vars
+      float ax, ay, az;
+      float gx, gy, gz;
+
+      if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+        IMU.readAcceleration(ax, ay, az);
+        IMU.readGyroscope(gx, gy, gz);
+
+        Serial.print(ax);
+        Serial.print('\t');
+        Serial.print(ay);
+        Serial.print('\t');
+        Serial.print(az);
+        Serial.print('\t');
+        Serial.print(gx);
+        Serial.print('\t');
+        Serial.print(gy);
+        Serial.print('\t');
+        Serial.println(gz);
+      }
+
+      // if the remote device wrote to the characteristic,
+      // use the value to control the LED:
+      if (switchCharacteristic.written()) {
+        switch (switchCharacteristic.value()) {   // any value other than 0
+          case 01:
+            Serial.println("sending IMU values");
+            rapidBlink();
+            // write to phone, but this doesn't work, need to figure out
+            // 1. how to write decimals
+            // 2. how to write multiple values
+            switchCharacteristic.writeValue(ax);
+            switchCharacteristic.writeValue(ay);
+            switchCharacteristic.writeValue(az);
+            switchCharacteristic.writeValue(gx);
+            switchCharacteristic.writeValue(gy);
+            switchCharacteristic.writeValue(gz);
+            break;
+          default:
+            Serial.println(F("LED off"));
+            digitalWrite(LED_BUILTIN, LOW);          // will turn the LED off
+            break;
+        }
+      }
+    }
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+    digitalWrite(LED_BUILTIN, LOW);         // will turn the LED off
+  }
+}
+
+void rapidBlink() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
