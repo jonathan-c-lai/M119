bd883c35b1701ef19f37c8180b63e020199b5364
Jonathan Lai <78984450+jonathan-c-lai@users.noreply.github.com>
2022-11-21 18:26:45 -0800

    m5 single player z axis

---
+++ Assignment 5/IMU_Bluetooth_Stream/IMU_Bluetooth_Stream.ino
@@ -0,0 +1,95 @@
+#include <ArduinoBLE.h>
+#include <Arduino_LSM6DS3.h>
+
+// used the skeleton code from professor's guide:
+// https://github.com/ucla-hci/m119/blob/main/m2b_peripheral/m2b_peripheral.ino
+// provided as a resource to help us complete this assignment
+
+#define BLE_UUID_ACCELEROMETER_SERVICE "1866" //1101
+#define BLE_UUID_ACCELEROMETER_X "2101"
+#define BLE_UUID_ACCELEROMETER_Y "2102"
+#define BLE_UUID_ACCELEROMETER_Z "2103"
+
+BLEService accelerometerService(BLE_UUID_ACCELEROMETER_SERVICE);
+
+BLEFloatCharacteristic accelerometerCharacteristicX(BLE_UUID_ACCELEROMETER_X, BLERead | BLENotify);
+BLEFloatCharacteristic accelerometerCharacteristicY(BLE_UUID_ACCELEROMETER_Y, BLERead | BLENotify);
+BLEFloatCharacteristic accelerometerCharacteristicZ(BLE_UUID_ACCELEROMETER_Z, BLERead | BLENotify);
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // initialize IMU
+  if (!IMU.begin()) {
+    Serial.println("IMU initialization failed");
+
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in g's");
+  Serial.println("X\tY\tZ");
+
+  // initialize bluetooth
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy failed!");
+
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("Nano 33 IoT");
+  BLE.setAdvertisedService(accelerometerService);
+
+  // add the characteristics to the service
+  accelerometerService.addCharacteristic(accelerometerCharacteristicX);
+  accelerometerService.addCharacteristic(accelerometerCharacteristicY);
+  accelerometerService.addCharacteristic(accelerometerCharacteristicZ);
+
+  // add service
+  BLE.addService(accelerometerService);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE Accelerometer Peripheral");
+}
+
+void loop() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+      // setup IMU vars
+      float ax, ay, az;
+
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(ax, ay, az);
+
+        Serial.print(ax);
+        Serial.print('\t');
+        Serial.print(ay);
+        Serial.print('\t');
+        Serial.println(az);
+        accelerometerCharacteristicX.writeValue(ax);
+        accelerometerCharacteristicY.writeValue(ay);
+        accelerometerCharacteristicZ.writeValue(az);
+      }
+    }
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
+}
